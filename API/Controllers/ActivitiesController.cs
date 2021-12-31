7950988d264fb763a678bc75acb8794aea178f0c
HeapOverfl0w <jgregorybest@gmail.com>
2021-12-31 15:26:44 -0500

    Initial Checkin After Section 7

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,42 @@
+using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Domain;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new Application.Activities.List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Application.Activities.Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok( await Mediator.Send(new Application.Activities.Create.Command { Activity = activity }) );
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Application.Activities.Edit.Command() { Activity = activity }));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Application.Activities.Delete.Command() { Id = id }));
+        }
+    }
+}
